ff17a62289060eef9551dee636ba03b865fbf77d
ManuelRdrguez <145555749+ManuelRdrguez@users.noreply.github.com>
2023-09-20 12:54:01 +0200

    Add files via upload

---
+++ HellowordlWeb/src/main/java/prueba/HelloWorld.java
@@ -0,0 +1,46 @@
+package prueba;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class HelloWorld
+ */
+public class HelloWorld extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public HelloWorld() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter(); 
+		//ejercicio 1
+		String nombre = request.getParameter("nombre"); 
+		String apellido = request.getParameter("apellido"); 
+        out.print("Hola, " + nombre + " " + apellido + "!");
+
+		
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
